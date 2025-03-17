1e3aa1133c35e2c5fc496f047b70d196d0a635ec
Artyom Chshyogolev <artyom.chshyogolev@gmail.com>
2025-03-17 22:38:49 +0500

    Initial Commit

---
+++ RemoteGamepad/RemoteGamepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteGamepadApp.swift
+//  RemoteGamepad
+//
+//  Created by artchsh on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RemoteGamepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
